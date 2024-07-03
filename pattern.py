6db0a383c0d7de4df871e5c90d5d70b537826c9f
Yashas_J <yashasjraj@gmail.com>
2024-07-03 12:21:36 +0530

    updated pattern.py with new methods

--- pattern.py
+++ pattern.py
@@ -127,3 +127,12 @@ def nNumberTriangle(n: int) -> None:
             print(j,end=" ")
         print()
             
+def nStarTriangle(n: int) -> None:
+    for i in range(n):
+        for j in range(i):
+            print(" ",end="")
+        for k in range((n*2)-(2*i)-1):
+            print("*",end="")
+        for l in range(i):
+            print(" ",end="")
+        print()
