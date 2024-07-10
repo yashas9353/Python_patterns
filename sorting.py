bb317bcf3b230e639577e870f5eb9837e815867a
Yashas_J <yashasjraj@gmail.com>
2024-07-10 09:36:53 +0530

    added mergeSort algo

--- sorting.py
+++ sorting.py
@@ -33,5 +33,34 @@ def insertion_sort(arr,n):
             j -= 1
     return arr
 
-li = [2,6,3,1,15,1,10]
-print(insertion_sort(li,len(li)))
+# MergeSort 
+class Solution:
+    def merge(self,arr, l, m, r): 
+        temp = []
+        left = l
+        right = m+1
+        while left<=m and right<=r:
+            if arr[left]<=arr[right]:
+                temp.append(arr[left])
+                left+=1
+            else:
+                temp.append(arr[right])
+                right+=1
+        
+        while left<=m:
+            temp.append(arr[left])
+            left+=1
+        while right<=r:
+            temp.append(arr[right])
+            right+=1
+        
+        for i in range(l,r+1):
+            arr[i] = temp[i-l]
+            
+    def mergeSort(self,arr, l, r):
+        if l >= r:
+            return 
+        mid = (l+r)//2
+        self.mergeSort(arr,l,mid)
+        self.mergeSort(arr,mid+1,r)
+        self.merge(arr,l,mid,r)
