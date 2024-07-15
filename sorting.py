397ce9f5b3c6ecc9c0230ec2e88285c27e41e048
Yashas_J <yashasjraj@gmail.com>
2024-07-15 12:00:41 +0530

    added quick sort using python

--- sorting.py
+++ sorting.py
@@ -64,3 +64,30 @@ class Solution:
         self.mergeSort(arr,l,mid)
         self.mergeSort(arr,mid+1,r)
         self.merge(arr,l,mid,r)
+
+# Quick Sort 
+
+class Solution:
+    #Function to sort a list using quick sort algorithm.
+    def quickSort(self,arr,low,high):
+        if low < high:
+            partition_pivot = self.partition(arr,low,high)
+            self.quickSort(arr,low,partition_pivot-1)
+            self.quickSort(arr,partition_pivot+1,high)
+        
+    
+    def partition(self,arr,low,high):
+        pivot = arr[low]
+        i = low
+        j = high
+        
+        while i<j:
+            while arr[i] <= pivot and i<=high-1:
+                i+=1
+            while arr[j] > pivot and j>=low+1:
+                j-=1
+            if i<j:
+                arr[i],arr[j] = arr[j],arr[i]
+        
+        arr[j],arr[low]=arr[low],arr[j]
+        return j
