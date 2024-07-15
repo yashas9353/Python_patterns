aa9489b31fab1b91ccb74f65c80218b81b92e9ed
Yashas_J <yashasjraj@gmail.com>
2024-07-15 14:00:06 +0530

    added removeDuplicate optimal approach code

--- array_easy_problem.py
+++ array_easy_problem.py
@@ -1,5 +1,6 @@
 from typing import List
 
+# Brute Force way
 def removeDuplicates(self, nums: List[int]) -> int:
     """
     Remove duplicates from a sorted list of integers and return the new length of the list.
@@ -23,3 +24,12 @@ def removeDuplicates(self, nums: List[int]) -> int:
         nums[index] = i
         index += 1
     return index
+
+# Optimal way 
+def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        for j in range(1,len(nums)):
+            if nums[j] != nums[i]:
+                nums[i+1] = nums[j]
+                i+=1
+        return i+1
